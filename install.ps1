b69033eb9748a2351b9ae71f9a7add86d93546fa
Pirated Software <145173541+piratedsoftware@users.noreply.github.com>
2024-03-18 13:21:07 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,20 @@
 # Local onde o arquivo será salvo temporariamente
-$tempFile = "C:\Users\admin\Desktop\setup.exe"
-Start-Process -FilePath $tempFile -Wait
+# $tempFile = "C:\Users\admin\Desktop\setup.exe"
+# Start-Process -FilePath $tempFile -Wait
+
+# URL do arquivo .exe a ser baixado
+$url = "https://github.com/piratedsoftware/program1/blob/main/hello.exe"
+
+# Local onde o arquivo será salvo temporariamente
+$tempFile = "$env:TEMP\seu-arquivo.exe"
+
+# Baixar o arquivo .exe
+Invoke-WebRequest -Uri $url -OutFile $tempFile
+
+# Verificar se o download foi bem-sucedido
+if (Test-Path $tempFile) {
+    # Executar o arquivo .exe
+    Start-Process -FilePath $tempFile -Wait
+} else {
+    Write-Host "Falha ao baixar o arquivo."
+}
